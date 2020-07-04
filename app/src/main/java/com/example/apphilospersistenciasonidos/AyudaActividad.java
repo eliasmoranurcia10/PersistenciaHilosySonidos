d6a914ac3883eb010c5b44d00fe6e0f04d40ca57
EliasMoran <yoexereliasmoranurcia@gmail.com>
2020-07-04 03:06:36 -0500

    VIDEO 59: Persistencia, hilos y sonidos II: cracion de layout activity_ayuda_actividad, crear el boton ayuda, llamar con Intent y agregarle texto

---
+++ app/src/main/java/com/example/apphilospersistenciasonidos/AyudaActividad.java
@@ -0,0 +1,14 @@
+package com.example.apphilospersistenciasonidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AyudaActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ayuda_actividad);
+    }
+}
--- app/src/main/java/com/example/apphilospersistenciasonidos/MainActivity.java
+++ app/src/main/java/com/example/apphilospersistenciasonidos/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.apphilospersistenciasonidos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //llamar al archivo AyudaAcividad
+    public void ayuda(View vista){
+        //Instanciar Intent para llamar otro layout, llamar a la clase AyudaActividad
+        Intent intencion = new Intent(this,AyudaActividad.class);
+        //Empezar la actividad
+        startActivity(intencion);
     }
 }
