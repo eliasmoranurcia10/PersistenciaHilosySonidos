79f6628ebbda8e5e0b87f1a7a3f797ddcb7ec2eb
EliasMoran <yoexereliasmoranurcia@gmail.com>
2020-09-07 10:55:59 -0500

    VIDEO 60: Fin de este video, creacion del boton back, definir dimensiones para otro ancho y configurar el app para horizontal

--- app/src/main/java/com/example/apphilospersistenciasonidos/AyudaActividad.java
+++ app/src/main/java/com/example/apphilospersistenciasonidos/AyudaActividad.java
@@ -3,6 +3,7 @@ package com.example.apphilospersistenciasonidos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AyudaActividad extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class AyudaActividad extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda_actividad);
+    }
+
+    public void volver(View vista){
+        onBackPressed();
     }
 }
